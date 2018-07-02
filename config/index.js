12aa3c1b67bc6720d4ef7dd89cec169a18604547
Young Sang Cho <youngsangcho@Youngs-MacBook-Pro-2.local>
2018-07-02 10:58:24 +0900

    build test

--- config/index.js
+++ config/index.js
@@ -41,9 +41,9 @@ module.exports = {
     index: path.resolve(__dirname, '../docs/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../docs'),
+    assetsRoot: path.resolve(__dirname, '../docs    '),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/cdls/',
 
     /**
      * Source Maps
